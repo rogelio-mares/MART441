c5f4ebf91989957401ffcda1b201a014ebeba0b4
rogelio-mares <77692713+rogelio-mares@users.noreply.github.com>
2022-02-14 22:29:44 -0700

    Add files via upload

---
+++ week2/readme.md
@@ -0,0 +1,3 @@
+# Week 2
+
+I thought the introduction to Java wasnt too bad the only problem I had was trying to figure out to get multiple lines in the console log. I didnt really figure it out so i just ended up making multiple console logs.
---
+++ week2/script.js
@@ -0,0 +1,3 @@
+console.log("3 of your favorite websites- Stussy, Nike, Twitter");
+console.log("3 of your favorite games- Zelda, Animal Crossing, Spiderman");
+console.log("Frank Ocean(Pop), The Weekend (r&b/soul), Bad Bunny (Urbano Latino)");
